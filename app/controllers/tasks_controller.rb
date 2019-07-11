eb7ceef3da3ba1f9477faaa52753aeb9d06b4048
川上　貴弘 <chorin@kawakamitakahironoMacBook-Pro.local>
2019-07-12 01:49:02 +0900

    add: 登録アクションを実装

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,37 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save
+    redirect_to tasks_path, notice: "タスク「#{task.name}」を登録しました。"
+  end
+
+  def edit
+    @task = task.find(params[:id])
+  end
+
+  def update
+    @task = task.find(params[:id])
+  end
+
+  def destroy
+    @task = task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
+end
